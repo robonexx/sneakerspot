07bad20c8599fb7615efc48ad3ab95dd5861ef9d
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-08-11 12:54:02 +0200

    fixes on data

--- constants/dummyData.ts
+++ constants/dummyData.ts
@@ -4,35 +4,35 @@ export const dummyProducts: Product[] = [
   {
     id: 1,
     name: "Laptop",
-    price: 1000,
+    price: 15689,
     image:
       "https://www.elgiganten.se/image/dv_web_D180001002817537/254421/macbook-air-13-m18256-2020-space-grey--pdp_zoom-3000--pdp_main-960.jpg",
   },
   {
     id: 2,
-    name: "Phone",
-    price: 500,
+    name: "IPhone 15",
+    price: 17999,
     image:
       "https://www.elgiganten.se/image/dv_web_D180001002838380/361879/iphone-13-5g-smartphone-128gb-midnight--pdp_zoom-3000--pdp_main-960.jpg",
   },
   {
     id: 3,
     name: "Air fryer",
-    price: 200,
+    price: 1499,
     image:
       "https://www.elgiganten.se/image/dv_web_D1800010021135650/514566/obh-nordica-easy-fry-precisionplus-airfryer-ag505ds0--pdp_zoom-3000--pdp_main-960.jpg",
   },
   {
     id: 4,
-    name: "Samsun 55' TV",
-    price: 1000,
+    name: "Samsung 55' TV",
+    price: 12999,
     image:
       "https://www.elgiganten.se/image/dv_web_D1800010021159012/497797/samsung-55-q60b-4k-qled-smart-tv-2022-calman-kalibrerad--pdp_zoom-3000--pdp_main-960.jpg",
   },
   {
     id: 5,
-    name: "Printer",
-    price: 300,
-    image: "https://picsum.photos/id/41/400/300",
+    name: "Playstation 5",
+    price: 6500,
+    image: "https://www.elgiganten.se/image/dv_web_D1800010021328565/581993/playstation-5-2x-dualsense-kontroller-paket--pdp_zoom-3000.jpg",
   },
 ];
