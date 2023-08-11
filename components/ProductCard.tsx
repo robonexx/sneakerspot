f84f8163e631b2a0174cf9c5cca191d9fc2f3d62
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-08-11 10:04:37 +0200

    productCard base done, re-design later

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,9 +6,9 @@ interface Props {
 }
 const ProductCard = (props: Props) => {
   return (
-    <div className='group relative cursor-pointer hover:shadow px-2 py-2 rounded'>
+    <div className='group relative cursor-pointer shadow px-2 py-2 rounded-lg hover:shadow-lg lg:h-80 lg:w-60'>
     {/* Changed bg to white, so the image fits and set it to contain.  */}
-    <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-white lg:aspect-none group-hover:opacity-75 lg:h-80 lg:w-60'>
+    <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-white lg:aspect-none group-hover:opacity-75 '>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={props.product.image}
@@ -18,7 +18,7 @@ const ProductCard = (props: Props) => {
     </div>
     <div className='mt-4 flex justify-between'>
       <div>
-        <h3 className='text-sm text-white px-2'>{props.product.name}</h3>
+        <h3 className='text-sm text-gray-800 px-2'>{props.product.name}</h3>
       </div>
       <p className='text-sm font-medium text-gray-400 px-2'>
         {props.product.price} kr
