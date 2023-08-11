0cc08d445e5be323eefa433df552e36f0ec9a529
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-08-11 15:08:24 +0200

    checkout page foundation, done

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import CartItemsCard from '@/components/CartItemsCard';
+import { TotalPriceSelector } from '@/store/features/cartSlice';
 import { useAppSelector } from '@/store/store';
 import React from 'react';
 
 const CheckoutPage = () => {
   const cartItems = useAppSelector((state) => state.cart.cartItems);
+  const totalPrice = useAppSelector(TotalPriceSelector);
   return (
     <div className='p-4 flex flex-col w-full gap-4 place-content-center'>
       {cartItems.map((item) => (
@@ -12,6 +14,8 @@ const CheckoutPage = () => {
           <CartItemsCard cartItem={item} />
         </div>
       ))}
+
+      <p className='p-2 text-black text-sm text-end w-full'>Total summa: {totalPrice} kr</p>
     </div>
   );
 };
