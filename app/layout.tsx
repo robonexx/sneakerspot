ecdbe2ea2314f6c0f5aadc3251b32f06eeb87d6c
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-09-13 17:37:24 +0200

    footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Providers from '@/components/Providers'
 import Header from '@/components/layouts/Header'
+import Footer from '@/components/layouts/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,8 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         <Providers>
           <Header>Nav</Header>
-        {children}
+          {children}
+          <Footer></Footer>
         </Providers>
       </body>
     </html>
