555683b05875315a3a691cecdfe72d6b52365c06
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-09-13 17:30:20 +0200

    header fix, pass children prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         <Providers>
-          <Header />
+          <Header>Nav</Header>
         {children}
         </Providers>
       </body>
--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,13 +1,15 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import Link from 'next/link';
 import CartBtn from '../CartBtn';
 
-const Header: FC<Props> = ({children}) => {
+type Props = {
+  children: string | JSX.Element | JSX.Element[] | ReactNode;
+};
+
+const Header: FC<Props> = ({ children }: Props) => {
   return (
     <header className='bg-gradient-to-r from-black to-indigo-900 shadow p-2 flex justify-between items-center sticky top-0 z-50'>
-      <Link className='text-white px-2' href={'/'}>
-        Home
-      </Link>
+      {children}
       <Link className='text-white py-2 px-2' href={'/checkout'}>
         <CartBtn />
       </Link>
